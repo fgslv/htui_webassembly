7a5c05afea4982e52c1f520699b4a1d1b95bf60e
Silvio Feig <silvio.feig@zesbo.de>
2020-03-03 11:44:37 +0100

    pass array data between js and cpp

--- cpp/function.cpp
+++ cpp/function.cpp
@@ -1,11 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
 #include <math.h>
 
 extern "C" {
-  int int_sqrt(int x) {
+  int int_sqrt(int x)
+  {
     return sqrt(x);
   }
 
-  extern float addNums(float *buffer, int bufSize) {
+  extern float addNums(float *buffer, int bufSize)
+  {
     float total = 0;
 
     for (int i=0; i<bufSize; i++) {
@@ -13,5 +17,29 @@ extern "C" {
     }
 
     return total;
+  }
+
+
+  extern void sortNums(uint8_t *buffer, int bufSize)
+  {
+    printf("sortNums: received %d nums to sort\n", bufSize);
+    for (int i=0; i<bufSize; i++) 
+      printf("%d, ", buffer[i]);
+    printf("\n");
+
+    uint8_t temp = 0;
+    for (int i=0; i<bufSize; i++) {
+      for (int j=i+1; j<bufSize; j++) {
+	if (buffer[i] > buffer[j]) {
+	  temp = buffer[i];
+	  buffer[i] = buffer[j];
+	  buffer[j] = temp;
+	}
+      }
+    }
+
+    for (int i=0; i<bufSize; i++) 
+      printf("%d, ", buffer[i]);
+    printf("\n");
   }
 }
