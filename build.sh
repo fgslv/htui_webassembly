b2576200a96f18fb012d8d821f811a5d496ee2d2
Silvio Feig <silvio.feig@zesbo.de>
2020-03-04 14:50:16 +0100

    add CMakeList.txt

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-rm -rf build
-mkdir build
-cd build
-emcc -o function.js ../cpp/function.cpp ../cpp/fib.cpp -s ASSERTIONS=1 -g4 --source-map-base http://localhost:8080/index.html -lm -O2 -Wc++11-extensions -s WASM=1 -s EXPORTED_FUNCTIONS="['_int_sqrt', '_addNums', '_sortNums', '_fib', '_fibNext', '_next_val', '_new_fib']" -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap']"
-mv function.js ../web/gen/
-mv function.wasm ../web/gen/
-mv function.wasm.map ../web/gen/
-cd .. && rm -rf build
+mkdir -p build; cd build
+cmake .. \
+  -DCMAKE_TOOLCHAIN_FILE=${EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_INSTALL_PREFIX=.
+make
